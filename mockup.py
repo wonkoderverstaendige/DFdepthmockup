60e443a01944b0424baab94971a2288ba9efdbaf
wonko <crangos@web.de>
2012-02-18 21:07:42 -0500

    saving tiles in dictionaries

--- mockup.py
+++ mockup.py
@@ -1,16 +1,27 @@
-import sys, os, Image
+import sys, os, Image, hashlib
+import numpy as np
+
 
 imgpath = 'img\\2'
 tilesize = 16
+tiles = []
+
+class growingdict(dict):
+    def __missing__(self, key):
+        self[key] = len(self) + 1
+        return self[key]
+
+tiledict = growingdict()
+imgdict = dict()
 
 for file in os.listdir(imgpath):
     if os.path.splitext(file)[1] == '.png':
         im = Image.open(os.path.join(imgpath, file))
-        width = im.size[0]
-        height = im.size[1]
-        
+
         w, h = im.size
         
+        # +1 because I screwed up cropping, delete these later!!!
+        #if not ((w % tilesize) + (h % tilesize)) == 0:
         if not ((w+1)%tilesize + (h+1)%tilesize) == 0:
             print "Image "+file+" size not multiple of " + str(tilesize)
             print "X: "+str(((w+1.)/tilesize))+" Y: "+str((h+1.)/tilesize)
@@ -19,8 +30,16 @@ for file in os.listdir(imgpath):
             n_vert_tiles = (w+1)/tilesize
             n_hori_tiles = (h+1)/tilesize
 
-            for tx in range(0, n_hori_tiles):
-                for ty in range(0, n_vert_tiles):
+            # ignore first/last column/line when looping, only get "content", not frame
+            for tx in range(1, n_hori_tiles-1):
+                for ty in range(1, n_vert_tiles-1):
                     newtile = im.crop((tx * tilesize, ty * tilesize, tx * tilesize + tilesize, ty * tilesize + tilesize))
-                    tile.save(os.path.join(imgpath, str(tx) + "-" + str(ty) + "_" + file))
-                
+                    
+                    imgdict[tiledict[hashlib.md5(newtile.tostring()).hexdigest()]] = newtile
+                    
+print len(tiledict)
+t = 0
+for tileimgkey in imgdict:
+    print os.path.join(imgpath, 'tiles', str(t))
+    imgdict[tileimgkey].save(os.path.join(imgpath, 'tiles', str(t)+'.png'),'PNG')
+    t += 1
